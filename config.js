5671ff26ae1169abb06bf0998b13decf69793aef
Junseo Oh <130203975+oasunryo@users.noreply.github.com>
2025-08-26 20:43:22 +0900

    Update main color and author information in config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const siteConfig = {
   username: "", // GitHub 사용자 이름
   repositoryName: "", // GitHub 저장소 이름
-  mainColor: "#3498db", // 사이트의 주 색상
+  mainColor: "#2B7849", // 사이트의 주 색상
   textColor: "#333333", // 기본 텍스트 색상
   blogTitle: "", // 블로그 제목
 };
@@ -13,9 +13,9 @@ const siteConfig = {
 const users = [
   {
     id: 0, // default author
-    username: "licat",
+    username: "Junseo Oh",
     company: "weniv",
-    position: "CEO",
+    position: "owner",
     img: "img/user/profile-licat.png",
   },
 ];
